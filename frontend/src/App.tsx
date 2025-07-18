de96b6d855a778387b7efe2eae73917884703b9d
DAVIN DO <davin4do@gmail.com>
2025-07-18 17:18:05 +0800

    Established Google Identity Services login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
-
+declare global {
+  interface Window {
+    google: any;
+  }
+}
 function App() {
-  const [count, setCount] = useState(0)
 
+  function handleCallbackResponse(response: any){
+    console.log("Encoded JWT ID token: " + response.credential);
+  }
+  useEffect(() => {
+    if (window.google) {
+      window.google.accounts.id.initialize({
+        client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+        callback: handleCallbackResponse
+      });
+
+      window.google.accounts.id.renderButton(
+        document.getElementById("signIn"),
+        {theme: "outline", size: "large"}
+      );
+    }
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div id="signIn"></div>
     </>
   )
 }
